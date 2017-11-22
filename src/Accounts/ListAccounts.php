f00624788eefb204537ee8684c9aa226a2c04620
Tom Cannaerts <tom.cannaerts@intracto.com>
2017-11-22 17:13:04 +0100

    Added support for asset_type parameter

--- src/Accounts/ListAccounts.php
+++ src/Accounts/ListAccounts.php
@@ -14,9 +14,55 @@ use TomCan\CombellApi\Common\AbstractCommand;
 class ListAccounts extends AbstractCommand
 {
 
-    public function __construct()
+    private $assetType;
+
+    public function __construct($assetType = "")
     {
         parent::__construct("get", "/v2/accounts");
+        $this->setAssetType($assetType);
+    }
+
+    public function prepare()
+    {
+        parent::prepare();
+
+        if ($this->assetType !== "") {
+            $qs = "asset_type=" . $this->assetType;
+            if ($this->getQueryString() === "") {
+                $this->setQueryString($qs);
+            } else {
+                $this->setQueryString($this->getQueryString() . '&' . $qs);
+            }
+        }
+
+    }
+
+    /**
+     * @return string
+     */
+    public function getAssetType()
+    {
+        return $this->assetType;
+    }
+
+    /**
+     * @param string $assetType
+     */
+    public function setAssetType($assetType)
+    {
+
+        if ($assetType == "") {
+            $this->assetType = "";
+        } else {
+
+            if (in_array($assetType, array("domain", "linux_hosting", "mysql", "dns", "mailbox"))) {
+                $this->assetType = $assetType;
+            } else {
+                throw new \InvalidArgumentException("Invalid asset_type specified");
+            }
+
+        }
+
     }
 
 }
