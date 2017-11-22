0ec6e12d57a97c434f0e7859926a357c01660dcf
Tom Cannaerts <mot@tom.be>
2017-11-22 23:06:56 +0100

    added TransferDomain call

---
+++ src/Domains/TransferDomain.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace TomCan\CombellApi\Domains;
+
+use TomCan\CombellApi\Common\AbstractCommand;
+
+class TransferDomain extends AbstractCommand
+{
+
+    private $domainname;
+    private $authcode;
+
+    public function __construct($domainname, $authcode)
+    {
+        parent::__construct("post", "/v2/domains/transfers");
+
+        $this->setDomainname($domainname);
+        $this->setAuthcode($authcode);
+    }
+
+    public function prepare()
+    {
+
+        $obj = new \stdClass();
+        $obj->domain_name = $this->domainname;
+        $obj->auth_code = $this->authcode;
+
+
+        $this->setBody(
+            json_encode($obj)
+        );
+
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDomainname()
+    {
+        return $this->domainname;
+    }
+
+    /**
+     * @param mixed $domainname
+     */
+    public function setDomainname($domainname)
+    {
+        $this->domainname = $domainname;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAuthcode()
+    {
+        return $this->authcode;
+    }
+
+    /**
+     * @param mixed $authcode
+     */
+    public function setAuthcode($authcode)
+    {
+        $this->authcode = $authcode;
+    }
+
+}
