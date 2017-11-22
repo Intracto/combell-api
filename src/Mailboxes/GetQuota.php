f6f9db92a1f761e873d76c44fee9f4caf68b1b54
Tom Cannaerts <mot@tom.be>
2017-11-22 22:12:39 +0100

    added GetQuota call

---
+++ src/Mailboxes/GetQuota.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace TomCan\CombellApi\Mailboxes;
+
+use TomCan\CombellApi\Common\AbstractCommand;
+
+class GetQuota extends AbstractCommand
+{
+
+    /**
+     * @var string
+     */
+    private $domainname;
+
+    public function __construct($domainname)
+    {
+        parent::__construct("get", "/v2/mailboxes/{domainname}/quota");
+
+        $this->domainname = $domainname;
+
+    }
+
+    public function prepare()
+    {
+        $this->setEndPoint("/v2/mailboxes/" . $this->domainname . "/quota");
+    }
+
+    /**
+     * @return string
+     */
+    public function getDomainname(): string
+    {
+        return $this->domainname;
+    }
+
+    /**
+     * @param string $domainname
+     */
+    public function setDomainname(string $domainname)
+    {
+        $this->domainname = $domainname;
+    }
+
+}
