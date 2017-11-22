bfc60eb644f69030bd97742eb7bb47e17adbdd00
Tom Cannaerts <mot@tom.be>
2017-11-22 01:06:32 +0100

    Main class that will handle execution of API commands

---
+++ src/Common/Api.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tom
+ * Date: 21/11/2017
+ * Time: 22:58
+ */
+
+namespace TomCan\CombellApi\Common;
+
+
+class Api
+{
+
+    private $apiKey;
+    private $apiSecret;
+    private $adapter;
+
+    /**
+     * Api constructor.
+     * @param $apiKey
+     * @param $apiSecret
+     * @param $adapter
+     */
+    public function __construct($apiKey, $apiSecret, $adapter)
+    {
+        $this->apiKey = $apiKey;
+        $this->apiSecret = $apiSecret;
+        $this->adapter = $adapter;
+    }
+
+    public function ExecuteCommand(AbstractCommand $command) {
+
+        $command->prepare();
+
+        $headers = array(
+            "Authorization" => $this->getHmacHeader($command),
+            "Accept" => "application/json",
+        );
+
+        $ret = $this->adapter->Call(strtoupper($command->getMethod()), "https://api.combell.com" . $command->getEndPoint() . ( $command->getQueryString() !== '' ? '?' . $command->getQueryString() : "" ), $headers, $command->getBody());
+
+        return $ret;
+
+    }
+
+    private function getHmacHeader(AbstractCommand $command) {
+
+        $timestamp = time();
+        $nonce = uniqid();
+        $input =    $this->apiKey .
+                    strtolower($command->getMethod()) .
+                    urlencode($command->getEndPoint() . ($command->getQueryString() !== '' ? '?' . $command->getQueryString() : '')) .
+                    $timestamp .
+                    $nonce .
+                    ($command->getBody() !== '' ? base64_encode(md5($command->getBody(), true)) : '');
+        $hmac = base64_encode(hash_hmac('sha256', $input, $this->apiSecret, true));
+
+        return "hmac " . $this->apiKey . ":" . $hmac . ":" . $nonce . ":" . $timestamp;
+
+    }
+
+}
