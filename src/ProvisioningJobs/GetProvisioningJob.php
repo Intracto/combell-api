b3411f09c54130caf323f0be9a0e93121823411e
Tom Cannaerts <mot@tom.be>
2018-01-24 22:34:25 +0100

    Extract resourcelinks from response

--- src/ProvisioningJobs/GetProvisioningJob.php
+++ src/ProvisioningJobs/GetProvisioningJob.php
@@ -9,6 +9,9 @@ class GetProvisioningJob extends AbstractCommand
 
     private $job_id;
 
+    private $status = '';
+    private $resource_links = array();
+
     public function __construct($job_id)
     {
         parent::__construct("get", "/v2/provisioningjobs/{jobid}");
@@ -20,6 +23,30 @@ class GetProvisioningJob extends AbstractCommand
     public function prepare()
     {
         $this->setEndPoint("/v2/provisioningjobs/" . $this->job_id);
+    }
+
+    public function processResponse($response)
+    {
+
+        if ($response['status'] == 200) {
+            $this->status = $response['body']['status'];
+        }
+
+        if ($response['status'] == 201) {
+            $this->status = 'finished';
+            $this->resource_links = array();
+            foreach ($response['body']->resource_links as $link) {
+                $matches = array();
+                if (preg_match('/\/([a-z]+)\/([^\/]+)$/', $link, $matches)) {
+                    $this->resource_links[] = array(
+                        'type' => $matches[1],
+                        'id' => $matches[2],
+                    );
+                }
+            }
+        }
+
+        return $response;
     }
 
     /**
@@ -36,6 +63,14 @@ class GetProvisioningJob extends AbstractCommand
     public function setJobId($job_id)
     {
         $this->job_id = $job_id;
+    }
+
+    /**
+     * @return array
+     */
+    public function getResourceLinks(): array
+    {
+        return $this->resource_links;
     }
 
 }
