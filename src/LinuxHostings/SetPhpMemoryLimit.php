d3ae663beaf5677809a8b395daef0640e91a0a63
Tom Cannaerts <mot@tom.be>
2018-01-24 23:31:31 +0100

    Added PHP Memory limit and apcu memory settings

---
+++ src/LinuxHostings/SetPhpMemoryLimit.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace TomCan\CombellApi\LinuxHostings;
+
+use TomCan\CombellApi\Common\AbstractCommand;
+
+class SetPhpMemoryLimit extends AbstractCommand
+{
+
+    private $domainname;
+    private $memorylimit;
+
+    public function __construct($domainname, $memorylimit)
+    {
+        parent::__construct("put", "/v2/linuxhostings/{domainname}/phpsettings/memorylimit");
+
+        $this->setDomainname($domainname);
+        $this->setMemorylimit($memorylimit);
+    }
+
+    public function prepare()
+    {
+
+        $this->setEndPoint("/v2/linuxhostings/".$this->domainname."/phpsettings/memorylimit");
+
+        $obj = new \stdClass();
+        $obj->memory_limit = $this->memorylimit;
+
+        $this->setBody(
+            json_encode($obj)
+        );
+
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDomainname()
+    {
+        return $this->domainname;
+    }
+
+    /**
+     * @param mixed $domainname
+     */
+    public function setDomainname($domainname)
+    {
+        $this->domainname = $domainname;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMemorylimit()
+    {
+        return $this->memorylimit;
+    }
+
+    /**
+     * @param mixed $memorylimit
+     */
+    public function setMemorylimit($memorylimit)
+    {
+        $this->memorylimit = $memorylimit;
+    }
+
+}
