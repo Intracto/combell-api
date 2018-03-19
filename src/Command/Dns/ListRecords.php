105ff7e906b79601e894d9fb4d805eef29c71f2c
Tom Cannaerts <mot@tom.be>
2018-03-19 23:16:55 +0100

    Implemented ListRecords call

---
+++ src/Command/Dns/ListRecords.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace TomCan\CombellApi\Command\Dns;
+
+use TomCan\CombellApi\Command\AbstractCommand;
+use TomCan\CombellApi\Structure\Dns\AbstractDnsRecord;
+
+class ListRecords extends AbstractCommand
+{
+
+    private $domainname;
+
+    public function __construct($domainname)
+    {
+        parent::__construct("get", "/v2/dns/{domainname}/records");
+        $this->setDomainname($domainname);
+    }
+
+    public function prepare()
+    {
+        $this->setEndPoint('/v2/dns/'.$this->domainname.'/records');
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDomainname()
+    {
+        return $this->domainname;
+    }
+
+    /**
+     * @param mixed $domainname
+     */
+    public function setDomainname($domainname)
+    {
+        $this->domainname = $domainname;
+    }
+
+    public function processResponse($response)
+    {
+
+        $records = array();
+        foreach ($response['body'] as $record) {
+            $className = "\\TomCan\\CombellApi\\Structure\\Dns\\Dns" . $record->type . "Record";
+            switch ($record->type) {
+                case "A":
+                case "NS":
+                case "TXT":
+                case "CNAME":
+                case "SOA":
+                    $rec = new $className($record->id, $record->record_name, $record->ttl, $record->content);
+                    break;
+                case "MX":
+                    $rec = new $className($record->id, $record->record_name, $record->ttl, $record->content, $record->priority);
+                    break;
+                case "SRV":
+                    $rec = new $className($record->id, $record->record_name, $record->ttl, $record->service, $record->target, $record->protocol, $record->priority, $record->port, $record->weight);
+                    break;
+                default:
+                    throw new \Exception("Unknown DNS record type " . $record->type);
+            }
+            $records[] = $rec;
+        }
+
+        $response['response'] = $records;
+        return $response;
+
+    }
+
+}
