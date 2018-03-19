101f0f73014592cca197a07a18a64b58ce2e4ffa
Tom Cannaerts <mot@tom.be>
2018-03-20 00:28:20 +0100

    Added response structures

--- src/Command/Domains/ListDomains.php
+++ src/Command/Domains/ListDomains.php
@@ -3,6 +3,7 @@
 namespace TomCan\CombellApi\Command\Domains;
 
 use TomCan\CombellApi\Command\AbstractCommand;
+use TomCan\CombellApi\Structure\Domains\Domain;
 
 class ListDomains extends AbstractCommand
 {
@@ -10,6 +11,20 @@ class ListDomains extends AbstractCommand
     public function __construct()
     {
         parent::__construct("get", "/v2/domains");
+    }
+
+    public function processResponse($response)
+    {
+
+        $domains = array();
+        foreach ($response['body'] as $domain) {
+            $dom = new Domain($domain->domain_name, $domain->expiration_date, $domain->will_renew);
+            $domains[] = $dom;
+        }
+
+        $response['response'] = $domains;
+        return $response;
+
     }
 
 }
