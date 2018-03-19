c12ebb8ac2e6e68530cc63ff8e7adbc5fc7c3239
Tom Cannaerts <mot@tom.be>
2018-03-19 22:21:16 +0100

    Created A record structure

---
+++ src/Structure/Dns/DnsARecord.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace TomCan\CombellApi\Structure\Dns;
+
+
+class DnsARecord extends AbstractDnsRecord
+{
+
+    private $content;
+
+    /**
+     * DnsARecord constructor.
+     * @param $content
+     */
+    public function __construct($id = "", $hostname = "", $ttl = 3600, $content)
+    {
+        parent::__construct($id, 'A', $hostname, $ttl);
+        $this->setContent($content);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param mixed $content
+     */
+    public function setContent($content)
+    {
+        if ($filtered = filter_var($content, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
+            $this->content = $filtered;
+        } else {
+            throw new \InvalidArgumentException("Not a valid IPv4 address");
+        }
+    }
+
+}
