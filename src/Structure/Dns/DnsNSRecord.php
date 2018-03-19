88257b875cf81654f6981a290f51749fd8d9461e
Tom Cannaerts <mot@tom.be>
2018-03-19 23:04:09 +0100

    NS type also exists

---
+++ src/Structure/Dns/DnsNSRecord.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace TomCan\CombellApi\Structure\Dns;
+
+
+class DnsNSRecord extends AbstractDnsRecord
+{
+
+    private $content;
+
+    /**
+     * DnsNSRecord constructor.
+     * @param $id
+     * @param $hostname
+     * @param $ttl
+     * @param $content
+     */
+    public function __construct($id = "", $hostname = "", $ttl = 3600, $content)
+    {
+        parent::__construct($id, 'NS', $hostname, $ttl);
+        $this->setContent($content);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param mixed $content
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+
+    public function getObject()
+    {
+        $obj = parent::getObject();
+        $obj->content = $this->getContent();
+        return $obj;
+    }
+
+}
