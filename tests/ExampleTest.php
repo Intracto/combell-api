97dbb880afb5f08e9bc00d81fef803a5f8eef9ee
Tom Van Looy <tom@ctors.net>
2019-02-02 11:17:40 +0100

    Import test framework / travis

---
+++ tests/ExampleTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class ExampleTest extends TestCase
+{
+    public function testTrueFalse()
+    {
+        $this->assertEquals(true, true);
+    }
+}
+ 
