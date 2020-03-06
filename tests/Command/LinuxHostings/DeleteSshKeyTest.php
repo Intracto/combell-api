5406320fc8b4b6f9c0c1584d9c149499cca30e8f
Tom Cannaerts <tom.cannaerts@intracto.com>
2020-03-06 10:41:51 +0100

    Added DeleteSshKey on LinuxHosting

---
+++ tests/Command/LinuxHostings/DeleteSshKeyTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Test\Command\Dns;
+
+use PHPUnit\Framework\TestCase;
+use TomCan\CombellApi\Adapter\AdapterInterface;
+use TomCan\CombellApi\Command\LinuxHostings\DeleteSshKey;
+use TomCan\CombellApi\Common\HmacGenerator;
+use TomCan\CombellApi\Common\Api;
+
+final class DeleteSshKeyTest extends TestCase
+{
+    public function testDeleteRecord(): void
+    {
+        $returnValue = [
+            'status' => 204,
+            'headers' => [
+                'Transfer-Encoding' => ['chunked'],
+                'X-RateLimit-Limit' => ['100'],
+                'X-RateLimit-Usage' => ['1'],
+                'X-RateLimit-Remaining' => ['99'],
+                'X-RateLimit-Reset' => ['60'],
+                'Date' => ['Sat, 02 Feb 2019 20:23:35 GMT'],
+            ],
+            'body' => '',
+        ];
+
+        $adapterStub = $this->createMock(AdapterInterface::class);
+        $headers = [
+            'Authorization' => 'hmac mocked',
+            'Accept' => 'application/json',
+            'Content-type' => 'application/json',
+        ];
+        $adapterStub->method('call')
+            ->with('DELETE', 'https://api.combell.com/v2/linuxhostings/example.com/ssh/keys/0123456789abcdef', $headers, '')
+            ->willReturn($returnValue);
+
+        $hmacGeneratorStub = $this->createMock(HmacGenerator::class);
+        $hmacGeneratorStub->method('getHeader')
+            ->willReturn('hmac mocked');
+        $api = new Api($adapterStub, $hmacGeneratorStub);
+
+        $cmd = new DeleteSshKey(
+            'example.com',
+            '0123456789abcdef'
+        );
+
+        $response = $api->executeCommand($cmd);
+        $this->assertNull($response);
+        $this->assertEquals('204', $api->getResponseCode());
+
+    }
+}
