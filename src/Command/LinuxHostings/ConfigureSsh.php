7afdf7eae3ecb6ad890c39c9e0bb5a21b7b490c0
Tom Cannaerts <tom.cannaerts@intracto.com>
2020-03-06 10:52:37 +0100

    Added ConfigureSsh on LinuxHosting

---
+++ src/Command/LinuxHostings/ConfigureSsh.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace TomCan\CombellApi\Command\LinuxHostings;
+
+use TomCan\CombellApi\Command\AbstractCommand;
+
+class ConfigureSsh extends AbstractCommand
+{
+    private $domainName;
+    private $enabled;
+
+    public function __construct(string $domainName, bool $enabled)
+    {
+        parent::__construct('put', '/v2/linuxhostings/{domainName}/ssh/configuration');
+
+        $this->domainName = $domainName;
+        $this->enabled = $enabled;
+    }
+
+    public function prepare(): void
+    {
+        $this->setEndPoint('/v2/linuxhostings/'.$this->domainName.'/ssh/configuration');
+
+        $obj = new \stdClass();
+        $obj->enabled = $this->enabled;
+
+        $this->setBody((string) json_encode($obj));
+    }
+
+    public function processResponse(array $response)
+    {
+    }
+}
