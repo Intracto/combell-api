66e3bcb617400ee808d2cdfdd889eae381218115
Tom Cannaerts <tom.cannaerts@intracto.com>
2020-03-06 10:52:37 +0100

    Added ConfigureSsh on LinuxHosting

---
+++ tests/Command/LinuxHostings/ConfigureSshTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Test\Command\LinuxHostings;
+
+use PHPUnit\Framework\TestCase;
+use TomCan\CombellApi\Adapter\AdapterInterface;
+use TomCan\CombellApi\Command\LinuxHostings\ConfigureSsh;
+use TomCan\CombellApi\Common\HmacGenerator;
+use TomCan\CombellApi\Common\Api;
+
+final class ConfigureSshTest extends TestCase
+{
+    public function testConfigureSsh(): void
+    {
+        $returnValue = [
+            'status' => 204,
+            'headers' => [
+                'Transfer-Encoding' => ['chunked'],
+                'Content-Type' => ['application/json; charset=utf-8'],
+                'X-RateLimit-Limit' => ['100'],
+                'X-RateLimit-Usage' => ['1'],
+                'X-RateLimit-Remaining' => ['99'],
+                'X-RateLimit-Reset' => ['60'],
+                'Date' => ['Sat, 02 Feb 2019 20:23:35 GMT'],
+            ],
+            'body' => '',
+        ];
+
+        $adapterStub = $this->createMock(AdapterInterface::class);
+        $headers = [
+            'Authorization' => 'hmac mocked',
+            'Accept' => 'application/json',
+            'Content-type' => 'application/json',
+        ];
+        $adapterStub->method('call')
+            ->with('PUT', 'https://api.combell.com/v2/linuxhostings/example.com/ssh/configuration', $headers, '{"enabled":false}')
+            ->willReturn($returnValue);
+
+        $hmacGeneratorStub = $this->createMock(HmacGenerator::class);
+        $hmacGeneratorStub->method('getHeader')
+            ->willReturn('hmac mocked');
+        $api = new Api($adapterStub, $hmacGeneratorStub);
+
+        $cmd = new ConfigureSsh('example.com', false);
+        $api->executeCommand($cmd);
+
+        $this->assertEquals('204', $api->getResponseCode());
+    }
+}
